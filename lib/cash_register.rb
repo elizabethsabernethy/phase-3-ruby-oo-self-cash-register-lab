be62677ab4265a60219deb422ec6d20f63b9597d
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-07 21:36:02 -0500

    commit tree 6ebdae3b95a06999628bd23e994b5be925ea0dd0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+class CashRegister
+
+   attr_accessor :discount, :total, :items, :last_transaction
+
+   def initialize(discount=20, total=0)
+    @discount = discount
+    @total = total
+    @items = []
+   end
+
+   def add_item(title, price, quantity=1)
+    @total = @total += (price*quantity)
+    amount = quantity
+        while amount > 0
+            @items << title
+            amount -= 1
+        end
+    @last_transaction = (price*quantity)
+   end
+
+   def apply_discount
+    self.total = (self.total - (self.total * (self.discount*0.01))).round()
+        if(self.total > 0)
+            "After the discount, the total comes to $#{total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+    @items
+    end
+
+    def void_last_transaction
+        @total = @total - @last_transaction
+    end
+end
